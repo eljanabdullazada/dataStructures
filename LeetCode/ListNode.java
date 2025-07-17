a31f3be800f9d727ee682ab3a8712d78821afc04
eljan_abdullazada <a3371977@gmail.com>
2025-07-17 22:48:20 +0400

    modify ListNode class by addin printList method. Solve leetcode question 83

--- LeetCode/ListNode.java
+++ LeetCode/ListNode.java
@@ -6,10 +6,19 @@ public class ListNode {
 
     ListNode(){}
     ListNode(int val) {
-        val = val;
+        this.val = val;
     }
     ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
+    }
+
+    public static void printList(ListNode head){
+        while(head != null){
+            System.out.print(head.val + " ");
+            if(head.next != null) System.out.print("-> ");
+            head = head.next;
+        }
+        System.out.println();
     }
 }
