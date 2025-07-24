e9bcfca4a216eff598e6a78462e58ba45f521cd9
eljan_abdullazada <a3371977@gmail.com>
2025-07-24 15:17:01 +0400

    leetcode question 94

---
+++ LeetCode/Question94.java
@@ -0,0 +1,40 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+
+    TreeNode(int val){
+        this.val = val;
+    }
+}
+
+public class Question94 {
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        List<Integer> result = inorderTraversal(root);
+        System.out.println(result);
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+
+    public static void inorder(TreeNode node, List<Integer> result){
+        if(node == null) return;
+
+        inorder(node.left, result);
+        result.add(node.val);
+        inorder(node.right, result);
+
+    }
+}
